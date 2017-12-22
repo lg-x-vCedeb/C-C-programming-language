8b29e2c702d33e9052bf0b60bb0dc75bfbb8a9b5
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2017-12-22 20:31:55 +0800

    Update **-1-1-hotel.h

--- **-1-1-hotel.h
+++ **-1-1-hotel.h
@@ -1,10 +1,11 @@
+//hotel.h -- usegotel.c && hotel.c 中的常量定义
 #define QUIT   5
 #define HOTEL1 80.00
 #define HOTEL2 125.00
 #define HOTEL3 155.00
 #define HOTEL4 200.00
 #define DISCOUNT 0.95
-#define STARS ************************************************
+#define STARS "************************************************"
 
 int menu();//给出选项列表
 int getnights();//返回预定的天数
