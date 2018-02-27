39616369fda76621f04b4bc8d97b6947b1aac047
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-02-27 17:09:50 +0800

    Create **pointersSample.c

---
+++ **pointersSample.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <string.h>
+void reverseR(int length, char *str);
+
+int main(){
+	char TestString[51];
+	printf("Please input a string (not over 50 characters)\n");
+	scanf_s("%s",&TestString,sizeof(TestString));
+	TestString[strlen(TestString)] = '\0';
+	if(strlen(TestString) <= 1){
+		printf("%s=>%s",TestString,TestString);
+	}
+	if(strlen(TestString) > 1){
+		printf("%s => ",TestString);
+		reverseR(strlen(TestString)-1,TestString);
+		printf("%s\n",TestString);
+	}
+	system("pause");
+	return 0;
+}
+
+void reverseR(int length, char *str){
+	int i = 0;
+	char c;
+	length = strlen(str);
+	while(i <= length / 2 - 1){
+		c = *(str + i);
+		*(str + i) = *(str + length - 1 - i);
+		*(str + length - 1 - i) = c;
+		i++;
+	}
+}
