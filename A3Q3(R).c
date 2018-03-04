57aada8cd267c8ac616a48944ee8896335497a46
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-03-04 21:57:10 +0800

    Create A3Q3(R).c

---
+++ A3Q3(R).c
@@ -0,0 +1,19 @@
+/*Write a program, to read a sequence of integers which end at -1. Store them in an array. Then
+call a sub-function average to calculate the average of the input integers (-1 excluded).
+Assume that the number of integers (-1 is not included) is no greater than 50.*/
+
+#include <stdio.h>
+#include <Windows.h>
+#include <string.h>
+#include <ctype.h>
+#define SEQLENGTH 51
+
+int main(){
+	int SeqOfInt[SEQLENGTH];
+	for(;;){
+		printf("Please input a sequence of integers.\n");
+		while(1){
+			scanf
+		}
+	}
+}
