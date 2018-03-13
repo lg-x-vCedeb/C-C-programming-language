be6f40b6879b357fd4eb491dc4d9b448099406d1
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-03-13 15:59:00 +0800

    Create DS&A_L3_stack.h

---
+++ DS&A_L3_stack.h
@@ -0,0 +1,21 @@
+typedef struct{
+	double* values;
+	int top;
+	int maxTop;
+}Stack;
+
+bool CreateStack(Stack *stack, int size);
+
+bool IsEmpty(Stack* stack);
+
+bool IsFull(Stack* stack);
+
+bool Top(Stack* stack, double* x);
+
+bool Push(Stack* stack, double x);
+
+bool Pop(Stack* stack, double* x);
+
+void DisplayStack(Stack* stack);
+
+void DestoryStack(Stack* stack);
