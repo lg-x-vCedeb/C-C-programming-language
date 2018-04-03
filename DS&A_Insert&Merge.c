5180172c8e91b4a8c23388149d90b26905af8126
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-04-03 19:56:32 +0800

    Update DS&A_Insert&Merge.c

--- DS&A_Insert&Merge.c
+++ DS&A_Insert&Merge.c
@@ -10,7 +10,8 @@ int main(void){
 	int *A1 = (int*)malloc(sizeof(int)*SIZE_OF_ARRAY);
 	for(int i = 0;i<SIZE_OF_ARRAY;i++)
 		A1[i] = rand() % 100000;
-	int *A2 = A1;
+	int *A2 = (int*)malloc(sizeof(int)*SIZE_OF_ARRAY);
+	A2 = A1;
 
 #if 0
 	for(int j = 0;j<10;j++)
@@ -45,8 +46,10 @@ void InsertionSort(int* A, int n){
 }
 
 void MergeSort(int* A, int left, int right){
+	int num;
+	int sign = 0;
 	int newleft, newright;
-	int center = (right-left)/2;
+	int center = (right+left)/2;
 	if(right - left != 1){
 		newright = center;
 		newleft = center + 1;
@@ -54,6 +57,23 @@ void MergeSort(int* A, int left, int right){
 		MergeSort(A,newleft,right);
 	}
 	else if (right - left == 1 || right == left){
-
+		sign = 1;
+	}
+	else if (sign == 1){
+		for(int j =0;j <= center - left;j++){
+			for(int k = 0;k <= center - left;k++){
+				if(A[left + j] > A[right - k]){
+					num = A[left + j];
+					A[left + j] = A[right - k];
+					A[right - k] = num;
+				}
+				else
+					continue;
+			}
+			center += right - center;
+			right += right - left;
+			if(center = SIZE_OF_ARRAY)
+				break;
+		}
 	}
 }
