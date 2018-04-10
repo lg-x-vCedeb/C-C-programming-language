d1358ab198b63953d18b0189e88013180d17ce64
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-04-10 21:34:47 +0800

    Update DS&A_Insert&Merge.c

--- DS&A_Insert&Merge.c
+++ DS&A_Insert&Merge.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <time.h>
 #include "T6.h"
-#define SIZE_OF_ARRAY 10
+#define SIZE_OF_ARRAY 1000
 
 int main(void){
 	int mnum;
@@ -17,21 +17,24 @@ int main(void){
 		A2[j] = mnum;
 	}
 
-//#if 0
+#if 0
 	for(int j = 0;j<10;j++)
 		printf("%d\t%d\n",A1[j],A2[j]);
-//#endif
+#endif
 
 	start1 = clock();
 	InsertionSort(A1,SIZE_OF_ARRAY);
 	end1 = clock();
+	printf("Int list sorted\n");
 
 	start2 = clock();
 	MergeSort(A2,0,SIZE_OF_ARRAY-1);
 	end2 = clock();
+	printf("Mer list sorted\n");
+#if 0
 	for(int j = 0;j<10;j++)
 		printf("%d\t%d\n",A1[j],A2[j]);
-
+#endif
 	printf("The time of insert sort is %f\nThe time of merge sort is %f\n",end1-start1,end2-start2);
 	system("pause");
 	return 0;
@@ -39,8 +42,8 @@ int main(void){
 
 void InsertionSort(int* A, int n){
 	int num;//Transition number
-	for(int i = 0; i < n - 1;i++){
-		for(int j = 0; j < n - 1;j++){
+	for(int i = 0; i < n;i++){
+		for(int j = 0; j < n;j++){
 			if(A[i] < A[j]){
 				num = A[i];
 				A[i] = A[j];
@@ -50,7 +53,7 @@ void InsertionSort(int* A, int n){
 				continue;
 		}
 	}
-	printf("list sorted!!\n");
+	//printf("list sorted!!\n");
 #if 0
 	for(int l = 0; l < n - 1;l++)
 		printf("%d ",A[l]);
@@ -69,8 +72,8 @@ void MergeSort(int* A, int left, int right){
 		MergeSort(A,newleft,right);
 	}
 	else if (right - left == 2 || right - left == 1 || right == left){
-		printf("%d\t%d\n",right,left);
-		while(right != SIZE_OF_ARRAY - 1){
+		//printf("%d\t%d\n",right,left);
+		do{
 			for(int j = 0;j <= center - left;j++){
 				for(int k = 0;k <= center - left;k++){
 					if(A[left + j] > A[right - k]){
@@ -85,41 +88,6 @@ void MergeSort(int* A, int left, int right){
 			}
 			center = right;
 			right += right - left;
-			if(center == SIZE_OF_ARRAY)
-				break;
-		}
+		}while(right < SIZE_OF_ARRAY);
 	}
-	/*
-	else if(sign == 1 && right - left == SIZE_OF_ARRAY - 1){
-		printf("ok?\n");
-		for(int j = 0;j <= center - left;j++){
-			for(int k = 0;k <= center - left;k++){
-				if(A[left + j] > A[right - k]){
-					num = A[left + j];
-					A[left + j] = A[right - k];
-					A[right - k] = num;
-					continue;
-				}
-				else
-					continue;
-			}
-		}
-	}
-	else if (sign == 1){
-		for(int j = 0;j <= center - left;j++){
-			for(int k = 0;k <= center - left;k++){
-				if(A[left + j] > A[right - k]){
-					num = A[left + j];
-					A[left + j] = A[right - k];
-					A[right - k] = num;
-					continue;
-				}
-				else
-					continue;
-			}
-			
-		}
-		
-	}*/
-	
 }
